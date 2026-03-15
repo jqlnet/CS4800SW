a5187d72b0fe45d6f8523cde536ebdc05fcedaa9
jqlnet <toniliang@cpp.edu>
2026-03-15 15:34:40 -0700

    uploaded unzipped spring.io file

---
+++ ReceiptTracker/src/main/java/edu/cpp/cs4800/ReceiptTracker/ReceiptTrackerApplication.java
@@ -0,0 +1,13 @@
+package edu.cpp.cs4800.ReceiptTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptTrackerApplication.class, args);
+	}
+
+}
---
+++ ReceiptTracker/src/test/java/edu/cpp/cs4800/ReceiptTracker/ReceiptTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cpp.cs4800.ReceiptTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
